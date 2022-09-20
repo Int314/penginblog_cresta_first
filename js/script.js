32730159eb918f2814858ffc96eb9d24a08320a0
username <15305383+Int314@users.noreply.github.com>
2022-09-20 20:22:03 +0900

    initial commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(".burger-btn").on("click", function () {
+  $(".burger-btn").toggleClass("close");
+  $(".nav-wrapper").toggleClass("close");
+  $("body").toggleClass("noscroll");
+});
+
+if ($(window).width() < 768) {
+  $(".nav-item>a").on("click", function () {
+      $(".nav-wrapper").removeClass("close");
+      $(".nav-wrapper").fadeOut(500);
+    $(".burger-btn").removeClass("close");
+    $("body").removeClass("noscroll");
+  });
+}
